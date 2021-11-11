0a4d9a9a1c84029e1b449f1c08daac4723b0f47a
David Kassin <david.j.kassin@gmail.com>
2021-11-10 21:11:39 -0700

    feat: Add date_format method

--- lib/key.rb
+++ lib/key.rb
@@ -9,4 +9,12 @@ module KeyEnumerables
   def generate_random_key
     generate_random.rjust(5,"0")
   end
+
+  def date_format
+  date = Date.today.to_s.split('-')
+  order = [2,1,0]
+  date[0].slice!(0..1)
+  date_key = date.values_at *order
+  date_key.join('')
+  end
 end
