31c12b3ea699da10a951c436b0dab7f098de8949
David Kassin <david.j.kassin@gmail.com>
2021-11-10 19:18:16 -0700

    test: Can show it can generate a random number and generate a number with length of 5

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,3 +1,4 @@
+require 'date'
 require './lib/enigma'
 require 'simplecov'
 SimpleCov.start
@@ -8,6 +9,7 @@ describe Enigma do
 
     expect(enigma).to be_a Enigma
     expect(enigma.random_key.length).to eq 5
+    expect(enigma.date).to be_a String
   end
 
   it 'can encrypt' do
@@ -21,5 +23,16 @@ describe Enigma do
     expect(enigma.encrypt("hello world")).to eq (expected)
   end
 
+  it 'can generate_random number as a string' do
+    enigma = Enigma.new
+
+    expect(enigma.generate_random).to be_a String
+  end
+
+  it 'can generate_random number as a string with a length of 5' do
+    enigma = Enigma.new
+
+    expect(enigma.generate_random_key.length).to be 5
+  end
 
 end
