37e0efa01c2c8e76f0d8cc0b3a65192f3e839479
David Kassin <david.j.kassin@gmail.com>
2021-11-10 19:05:12 -0700

    feat: Add method to generate random key on initialize

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,12 +1,18 @@
+require_relative './key'
+
 class Enigma
+  include KeyEnumerables
+
+  attr_reader :random_key
 
   def initialize()
+    @random_key = generate_random_key
   end
 
-  def encrypt(message,key_input = "02715", date_input = "040895")
+  def encrypt(message,key_input = @random_key, date_input = "040895")
     encrypt = {
       encryption: 0,#encryption_method(message)
-      key: key_input,
+      key: key_input, #should randomly generate a key
       date: date_input
     }
   end
