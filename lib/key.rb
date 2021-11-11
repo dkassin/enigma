24042dbeb47171e35fcd031fb37a91cb506b133e
David Kassin <david.j.kassin@gmail.com>
2021-11-10 19:18:47 -0700

    feat: Add generate_random helper method

--- lib/key.rb
+++ lib/key.rb
@@ -1,10 +1,12 @@
+require 'date'
+
 module KeyEnumerables
 
-  def generate_random_key
+  def generate_random
     number = rand(0..99999).to_s
-    if number.length != 5
-      number.rjust(5,"0")
-    end
-    number
+  end
+
+  def generate_random_key
+    generate_random.rjust(5,"0")
   end
 end
