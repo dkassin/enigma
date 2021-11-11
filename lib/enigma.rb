fc2fdd8be611bef0d6617de00a78996008b4dd73
David Kassin <david.j.kassin@gmail.com>
2021-11-10 18:30:50 -0700

    feat: Add Enigma class and encrypt method

---
+++ lib/enigma.rb
@@ -0,0 +1,13 @@
+class Enigma
+
+  def initialize()
+  end
+
+  def encrypt(message,key_input = "02715", date_input = "040895")
+    encrypt = {
+      encryption: 0,#encryption_method(message)
+      key: key_input,
+      date: date_input
+    }
+  end
+end
