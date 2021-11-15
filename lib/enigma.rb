ca77a0091f6a9806c4e5199704c590727bdc0a8d
David Kassin <david.j.kassin@gmail.com>
2021-11-15 10:39:56 -0700

    feat: Add encrypt method and decrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,14 +4,32 @@ require 'date'
 class Enigma
   include KeyEnumerables
 
-  attr_reader :random_key, :date, :encryptor
+  attr_reader :random_key, :date, :encryption
 
   def initialize()
     @random_key = generate_random_key
     @date = date_format
+    @encryption = nil
   end
 
   def encrypt(message,key_input = @random_key, date_input = @date)
-    Encryption.new(message,key_input,date_input)
+    @encryption = Encryption.new(message,key_input,date_input)
+    encrypt_hash =
+                  {
+                    encryption: @encryption.encryptor,
+                    key: key_input,
+                    date: date_input
+                  }
+  end
+
+  def decrypt(message,key_input = @random_key, date_input = @date)
+    @encryption = Encryption.new(message,key_input,date_input)
+    decrypt_hash =
+                  {
+                    encryption: @encryption.encryptor(-1),
+                    key: key_input,
+                    date: date_input
+                  }
+    require "pry"; binding.pry
   end
 end
