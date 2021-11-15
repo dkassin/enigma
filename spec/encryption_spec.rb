d69b860efa24035f145d6ed807ebdf65781e9f4e
David Kassin <david.j.kassin@gmail.com>
2021-11-15 14:36:45 -0700

    test: Show that split works on instance variable

--- spec/encryption_spec.rb
+++ spec/encryption_spec.rb
@@ -14,9 +14,10 @@ describe Encryption do
                 "B" => 27,
                 "C" => 73,
                 "D" => 20 }
+    expected_2 = ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"]
 
     expect(encryptor).to be_a Encryption
-    expect(encryptor.message).to eq ("hello world")
+    expect(encryptor.message).to eq (expected_2)
     expect(encryptor.key_input).to eq ("02715")
     expect(encryptor.date_input).to eq ("040895")
     expect(encryptor.final_shift).to eq(expected)
